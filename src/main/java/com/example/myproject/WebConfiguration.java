5a50ffce5fac045a3f21a9297503115349dc01d2
wclxs11 <xiaoshiwan123@gmail.com>
2019-06-21 21:11:28 +0800

    Add skeleton to home.html

--- src/main/java/com/example/myproject/WebConfiguration.java
+++ src/main/java/com/example/myproject/WebConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.myproject.comm.filter.SecurityFilter;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
 
 @Configuration
 public class WebConfiguration  {
@@ -20,6 +22,11 @@ public class WebConfiguration  {
         registration.setName("MyFilter");
         registration.setOrder(1);
         return registration;
+    }
+    
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
  
 }
